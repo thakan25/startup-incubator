bf22fd9e94e0f4e63d9e8b120dc07e6f2ff51967
sachin <sachin2opensource@gmail.com>
2021-12-17 12:00:15 +0530

    starting work on db integration

--- client/src/HomePage/Body.jsx
+++ client/src/HomePage/Body.jsx
@@ -4,23 +4,23 @@ import ProductPreview from '../Components/Product/ProductPreview';
 import * as constants from '../utils/constants';
 
 const Body = () => {
-    const dummyProducts  = constants.dummyProducts;
+    const dummyProducts = constants.dummyProducts;
     return (
-        <div style={{ margin: '2rem', padding: '2rem', border: '1px solid black', height:'80%', minHeight:'750px' }}>
+        <div style={{ margin: '2rem', padding: '2rem', border: '1px solid black', height: '80%', minHeight: '750px' }}>
             <Grid>
-               {dummyProducts.map(product => (
-                   <ProductPreview
-                    id={product.id}
-                     heading={product.heading}
-                     description={product.description}
-                     likes ={product.likes}
-                     image ={product.imagePath}
-                   />
-               ))}
+                {dummyProducts.map(product => (
+                    <ProductPreview
+                        id={product.id}
+                        heading={product.heading}
+                        description={product.description}
+                        likes={product.likes}
+                        image={product.imagePath}
+                    />
+                ))}
             </Grid>
-            
+
         </div>
-    )   
+    )
 }
 
 
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const {MongoClient} = require("mongodb");
+
+const uri = `mongodb+srv://sachinthakan001:helloMongoDB@cluster0.ovwhl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); //
 
